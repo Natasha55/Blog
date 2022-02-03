655b531bb5d4f662055e639237bf9b8bed13bdfa
Nata S <nserikova94@gmail.com>
2022-02-03 20:06:09 +0200

    update gitignore. in controllertest add save blog test and fetch by id. in repositorytest add test return title. in servicetest add fetch post by title

---
+++ src/test/java/com/blog/controller/BlogPostControllerTest.java
@@ -0,0 +1,35 @@
+package com.blog.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class BlogPostControllerTest {
+
+    @Test
+    void saveBlogPost() {
+    }
+
+    @Test
+    void fetchBlogPostList() {
+    }
+
+    @Test
+    void fetchBlogPostById() {
+    }
+
+    @Test
+    void deleteBlogPostById() {
+    }
+
+    @Test
+    void updateBlogPost() {
+    }
+
+    @Test
+    void fetchAllBlogPostByStar() {
+    }
+
+    @Test
+    void addBlogPostStar() {
+    }
+}
---
+++ src/test/java/com/blog/repository/BlogPostRepositoryTest.java
@@ -0,0 +1,11 @@
+package com.blog.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+class BlogPostRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
